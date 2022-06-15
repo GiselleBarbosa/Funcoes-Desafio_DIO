b159cbb38255063b07b39852fd29bd96b0fc247e
Giselle Barbosa <93397497+GiselleBarbosa@users.noreply.github.com>
2022-06-15 11:04:07 -0300

    :seedling: Funcoes_Desafios_DIO

---
+++ alunosAprovados.js
@@ -0,0 +1,35 @@
+const alunos = [
+  {
+    nome: "Giselle",
+    nota: 5,
+  },
+  {
+    nome: "Tiago",
+    nota: 2,
+  },
+  {
+    nome: "Marcos",
+    nota: 10,
+  },
+  {
+    nome: "Yago",
+    nota: 9,
+  },
+];
+
+function alunosAprovados(arr, media) {
+  let aprovados = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+        const { nota, nome } = arr[i];
+
+        if (arr[i].nota >= media) {
+         aprovados.push(nome);
+    }
+  }
+
+  return aprovados;
+}
+
+console.log(alunosAprovados(alunos, 7));
